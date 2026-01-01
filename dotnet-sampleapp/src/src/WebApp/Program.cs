657db84811d75a86f8d756a4491a70291be940db
saipriya-30 <saipriyamanduvada7@gmail.com>
2026-01-01 16:02:29 +0000

    Delete dotnet-sampleapp directory

--- dotnet-sampleapp/src/src/WebApp/Program.cs
+++
@@ -1,9 +0,0 @@
-var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddControllers();
-
-var app = builder.Build();
-
-app.MapControllers();
-app.MapGet("/", () => "Hello from .NET Sample App");
-
-app.Run();
